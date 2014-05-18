238b00b80e1782dd59fb115f2b97bccf79cb418f
Daniele Lenares <daniele.lenares@gmail.com>
2014-05-18 10:12:56 +0200

    First Release
    First Release

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-titlecounterjs
+titlecounter.js
 ==============
 
 A dependency-free javascript library to create a counter (down and up) that will be shown in your webpage title
+
+
+## How to use it
+Download the minified `titlecounter.min.js` and include it in your page:
+```
+<script src="titlecounter.min.js"></script>
+```
+
+#####Start a countup timer
+Starts the timer from 00:00
+```
+tc.countup();
+```
+#####Stop the timer
+Stops the previous started timer (doesn't clear the page title)
+```
+tc.stop();
+```
+#####Resume the timer
+Resume the previous stopped timer (doesn't work after clear() has been called)
+```
+tc.resume();
+```
+#####Clear the timer
+Stops the previous started timer and clear the page title
+```
+tc.clear();
+```
+
+## Changelog
+#### v0.1 (05-18-2014)
+* First release
+
+### License and Copyright
+jQuery GoUp! is licensed under [MIT](http://www.opensource.org/licenses/mit-license.php) license.
+
+(c) 2014 Daniele Lenares
+
+If you like my work, offer me a beer/coffee/cappuccino on [Gittip](https://www.gittip.com/Ryuk87/)
--- src/titlecounter.js
+++ src/titlecounter.js
@@ -1,20 +1,31 @@
+/*
+ *
+ * Copyright (c) 2014 Daniele Lenares (https://github.com/Ryuk87)
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ *
+ * Version 0.1
+ *
+ */
 /*global alert: false, confirm: false, console: false, Debug: false */
 
 
 var tc = function () {
     'use strict';
-    
+
     var counter,
+        mins = 0,
+        sec = 0,
+        countup = false,
+        countdown = false,
         original_title = document.title;
 
     return {
-        countdown: setInterval(function () {}),
-        start: function () {
-            var mins = 0,
-                sec = 0,
-                string_mins = '',
+        countup: function () {
+            var string_mins = '',
                 string_sec = '';
-            
+
+            countup = true;
+
             counter = setInterval(function () {
                 if (sec === 59) {
                     sec = 0;
@@ -34,22 +45,43 @@ var tc = function () {
                 } else {
                     string_sec = sec;
                 }
-                
+
                 document.title = '( ' + string_mins + ' : ' + string_sec + ' ) ' + original_title;
 
             }, 1000);
+        },
+        resume: function () {
+            if (counter !== undefined) {
+                if (countup === true) {
+                    this.countup();
+                } else if (countdown === false) {
+
+                }
+            } else {
+                console.info("No counter has started.");
+            }
         },
         stop: function () {
             if (counter !== undefined) {
                 clearInterval(counter);
                 counter = undefined;
+                countup = false;
+                countdown = false;
             } else {
                 console.info("stop() function has been called before a counter has started.");
             }
         },
-        clear: function() {
-            this.stop();
-            document.title = original_title;
+        clear: function () {
+            if (counter !== undefined) {
+                this.stop();
+                mins = 0;
+                sec = 0;
+                countup = false;
+                countdown = false;
+                document.title = original_title;
+            } else {
+                console.info("clear() function has been called before a counter has started.");
+            }
         }
     };
 }();
